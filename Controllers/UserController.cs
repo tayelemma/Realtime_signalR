928ae5caa4dd5d011aa56a9350d96c1771f9e260
Taye Lemma <lemmataye@yahoo.com>
2025-05-15 12:48:59 -0500

    First commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Realtime_signalR.Data;
+using Realtime_signalR.Models;
+
+namespace Realtime_signalR.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class UserController : ControllerBase
+    {
+    IUserRepository _userRepository;
+    IMapper _mapper;
+    public UserController(IUserRepository userRepository)
+    {
+      _userRepository = userRepository;
+      _mapper = new Mapper(new MapperConfiguration(cfg =>
+        {
+        cfg.CreateMap<Models.User, Dtos.UserDto>();
+      }));
+    }
+    [HttpGet("GetUser")]
+    public IEnumerable<User> GetUsers()
+    {
+       IEnumerable<User> users = _userRepository.GetUsers();
+       return users;
+    }
+    }
---
+++ Models/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace Realtime_signalR.Models
+{
+    public class Message
+    {
+        public int messageId { get; set; }
+        public string messageText { get; set; } = string.Empty;
+        public DateTime messageDate { get; set; }
+        public string messageTime { get; set; } = string.Empty;
+        public string messageImage { get; set; } = string.Empty;
+
+    }
+}
